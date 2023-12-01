d1e20f9c524db3df55036492e5a8a4549f27b015
Tom Hensel <github@jitter.eu>
2023-12-01 01:53:38 +0100

    add person holding the fork configuration :)

---
+++ users/tom/config.h
@@ -0,0 +1,12 @@
+// turbo/strobe/high
+#undef TACTICAL_LEVELS
+#define TACTICAL_LEVELS 150,152,(RAMP_SIZE+2)
+
+#undef SIMPLE_UI_ACTIVE
+#define SIMPLE_UI_ACTIVE 0 // advanced UI by default, because it's a dev board
+
+#undef DEFAULT_AUTOLOCK_TIME
+#define DEFAULT_AUTOLOCK_TIME 120 // autolock time in minutes, 0 = disabled
+
+#undef DEFAULT_AUTOLOCK_TIME
+#define USE_AUX_RGB_LEDS_WHILE_ON
