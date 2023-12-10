03004ebaa7eb24f9b7141962247e8774cacc29f1
gretel <code@jitter.eu>
2023-12-10 18:57:00 +0100

    disable tactical mode, sos_mode, version_check; cleanup

--- users/gretel/config.h
+++ users/gretel/config.h
@@ -1,9 +1,12 @@
-// turbo/strobe/high
-#undef TACTICAL_LEVELS
-#define TACTICAL_LEVELS 150,152,(RAMP_SIZE+2)
-
 #undef SIMPLE_UI_ACTIVE
-#define SIMPLE_UI_ACTIVE 0 // advanced UI by default, because it's a dev board
+#define SIMPLE_UI_ACTIVE 0 // advanced UI by default
 
-#undef DEFAULT_AUTOLOCK_TIME
-#define DEFAULT_AUTOLOCK_TIME 120 // autolock time in minutes, 0 = disabled
+#undef USE_TACTICAL_MODE
+// // turbo/strobe/high
+// #undef TACTICAL_LEVELS
+// #define TACTICAL_LEVELS 150,152,(RAMP_SIZE+2)
+
+#undef USE_SOS_MODE
+#undef USE_SOS_MODE_IN_BLINKY_GROUP
+
+#undef USE_VERSION_CHECK
