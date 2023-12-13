8bdbb5201695d280a1a0e5dda974d4d86802138d
gretel <code@jitter.eu>
2023-12-13 23:51:08 +0100

    add config making a ts21-v2 very simple

--- users/gretel/config.h
+++ users/gretel/config.h
@@ -4,10 +4,3 @@
 // turbo/strobe/high
 #undef TACTICAL_LEVELS
 #define TACTICAL_LEVELS 150,152,(RAMP_SIZE+2)
-
-// #undef USE_TACTICAL_MODE
-
-// #undef USE_SOS_MODE
-// #undef USE_SOS_MODE_IN_BLINKY_GROUP
-
-// #undef USE_VERSION_CHECK
---
+++ users/gretel/wurkkos/ts21-v2/config.h
@@ -0,0 +1,12 @@
+// for my dad :)
+
+#undef DEFAULT_THERM_CEIL
+#define DEFAULT_THERM_CEIL 45
+
+#undef SIMPLE_UI_ACTIVE
+#define SIMPLE_UI_ACTIVE 1 // advanced UI by default
+
+#undef DEFAULT_2C_STYLE_SIMPLE
+#define DEFAULT_2C_STYLE_SIMPLE 2
+
+#undef USE_AUTOLOCK
