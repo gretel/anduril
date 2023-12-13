d045b7b289ba586f21c245c6a2b26282e2ea15cf
gretel <code@jitter.eu>
2023-12-14 00:10:59 +0100

    refine configuration

--- users/gretel/wurkkos/ts21-v2/config.h
+++ users/gretel/wurkkos/ts21-v2/config.h
@@ -4,9 +4,26 @@
 #define DEFAULT_THERM_CEIL 45
 
 #undef SIMPLE_UI_ACTIVE
-#define SIMPLE_UI_ACTIVE 1 // advanced UI by default
+#define SIMPLE_UI_ACTIVE 1 // simple UI by default
+
+#undef SIMPLE_UI_STEPS
+#define SIMPLE_UI_STEPS 6
+
+#undef SIMPLE_UI_FLOOR
+#define SIMPLE_UI_FLOOR 20
+
+// Allow 3C (or 6C) in Simple UI (toggle smooth or stepped ramping)
+#undef USE_SIMPLE_UI_RAMPING_TOGGLE
+#define USE_SIMPLE_UI_RAMPING_TOGGLE
+
+// double click while on goes to full-power turbo, not ramp ceiling
+#undef DEFAULT_2C_STYLE
+#define DEFAULT_2C_STYLE 1
 
 #undef DEFAULT_2C_STYLE_SIMPLE
 #define DEFAULT_2C_STYLE_SIMPLE 2
+
+#undef INDICATOR_LED_DEFAULT_MODE
+#define INDICATOR_LED_DEFAULT_MODE ((0<<2) + 2) // high, off
 
 #undef USE_AUTOLOCK
